aaa006851ebb8fbafa17d6686aa0960a59b72a6b
ChrisHaj <chrishaj1191@gmail.com>
2024-05-13 21:15:42 +0300

    Update firebase-config.js

--- src/DB/firebase-config.js
+++ src/DB/firebase-config.js
@@ -8,13 +8,7 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAvaid1Zo9xSf11psXSlzqlYE4os7HW-ts",
-    authDomain: "yuvalimeventsmanager.firebaseapp.com",
-    projectId: "yuvalimeventsmanager",
-    storageBucket: "yuvalimeventsmanager.appspot.com",
-    messagingSenderId: "567875729069",
-    appId: "1:567875729069:web:1bfddc9b34dfe58a913824",
-    measurementId: "G-RMQSW8M91G"
+  
 };
 
 export default firebaseConfig
