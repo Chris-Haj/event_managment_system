72982e424c55d498e907271134c7c055556b3d7c
Chris-Haj <chrishaj1191@gmail.com>
2024-07-16 20:00:54 +0300

    Login fixed and added some new details to users

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,23 @@
-import React, {useState} from 'react';
-import LoginPage from '../ScreensJS/LoginPage.js'
-import EventsPage from '../ScreensJS/Events.js'
-import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+import React, { useState } from 'react';
+import AuthPage from '../ScreensJS/AuthPage';
+import EventsPage from '../ScreensJS/Events';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
-
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const handleLogin = () =>setIsLoggedIn(true);
+
+    const handleLogin = () => {
+        setIsLoggedIn(true);
+    };
 
     return (
         <Router>
             <Routes>
-                <Route path="/" element={isLoggedIn ? <Navigate replace to="/posts" /> : <LoginPage onLogin={handleLogin} />} />
+                <Route path="/" element={isLoggedIn ? <Navigate replace to="/events" /> : <AuthPage onLogin={handleLogin} />} />
                 <Route path="/events" element={isLoggedIn ? <EventsPage /> : <Navigate replace to="/" />} />
             </Routes>
         </Router>
-    )
-
+    );
 }
-
-
 
 export default App;
---
+++ src/ScreensJS/AuthPage.js
@@ -0,0 +1,232 @@
+import React, { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './AuthPage.css'; // Import custom CSS for additional styling
+import logo from '../Images/YovalimLogo.png';
+import db from '../DB/firebase'; // Ensure this is the correct path to your Firebase config
+
+function SignIn({ email, setEmail, password, setPassword, handleLogin, setHasAccount }) {
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleLogin}>
+                <div className="input-group mb-3">
+                    <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="input-group mb-3">
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary btn-block mb-4">Login</button>
+            </form>
+            <div className="forgot-password">Forgot password?</div>
+            <div className="toggle-form">
+                Don't have an account? <span className="sign-up-link" onClick={() => setHasAccount(false)}>Sign up here</span>
+            </div>
+        </div>
+    );
+}
+
+function SignUp({
+                    email,
+                    setEmail,
+                    password,
+                    setPassword,
+                    confirmPassword,
+                    setConfirmPassword,
+                    handleSignUp,
+                    setHasAccount,
+                    firstName,
+                    setFirstName,
+                    lastName,
+                    setLastName,
+                    birthDate,
+                    setBirthDate,
+                }) {
+    const currentDate = new Date().toISOString().split('T')[0]; // Get current date in YYYY-MM-DD format
+
+    return (
+        <div>
+            <h2>Sign Up</h2>
+            <form onSubmit={handleSignUp}>
+                <div className="input-group mb-3">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="First Name"
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="input-group mb-3">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Last Name"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="input-group mb-3">
+                    <input
+                        type="date"
+                        className="form-control"
+                        placeholder="Birth Date"
+                        value={birthDate}
+                        onChange={(e) => setBirthDate(e.target.value)}
+                        max={currentDate} // Set the max attribute to the current date
+                        required
+                    />
+                </div>
+                <div className="input-group mb-3">
+                    <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="input-group mb-3">
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="input-group mb-3">
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Confirm Password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary btn-block mb-4">Sign Up</button>
+            </form>
+            <div className="toggle-form">
+                Already have an account? <span className="sign-up-link" onClick={() => setHasAccount(true)}>Login here</span>
+            </div>
+        </div>
+    );
+}
+
+function AuthPage({ onLogin }) {
+    const [hasAccount, setHasAccount] = useState(true);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [birthDate, setBirthDate] = useState('');
+    const [error, setError] = useState('');
+    const auth = getAuth();
+
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            setError("Passwords do not match");
+            return;
+        }
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+            await setDoc(doc(db, 'users', user.uid), {
+                role: 'user',
+                firstName,
+                lastName,
+                birthDate,
+                email,
+            });
+            setHasAccount(true);
+            setEmail('');
+            setPassword('');
+            setConfirmPassword('');
+            setFirstName('');
+            setLastName('');
+            setBirthDate('');
+            setError('');
+        } catch (error) {
+            setError(`Error: ${error.code} ${error.message}`);
+        }
+    };
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+            const docRef = doc(db, 'users', user.uid);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists() && docSnap.data().role === 'admin') {
+                console.log('Admin logged in');
+                // Redirect to admin dashboard or handle accordingly
+            } else {
+                console.log('User logged in');
+                onLogin(); // Call onLogin to update login state
+            }
+        } catch (error) {
+            setError(`Error: ${error.code} ${error.message}`);
+        }
+    };
+
+    return (
+        <div className="container d-flex justify-content-center align-items-center min-vh-100">
+            <div className="card p-4" style={{ width: '400px' }}>
+                <img src={logo} alt="logo" className="mb-3 mx-auto d-block" style={{ width: '150px' }} />
+                {hasAccount ? (
+                    <SignIn
+                        email={email}
+                        setEmail={setEmail}
+                        password={password}
+                        setPassword={setPassword}
+                        handleLogin={handleLogin}
+                        setHasAccount={setHasAccount}
+                    />
+                ) : (
+                    <SignUp
+                        email={email}
+                        setEmail={setEmail}
+                        password={password}
+                        setPassword={setPassword}
+                        confirmPassword={confirmPassword}
+                        setConfirmPassword={setConfirmPassword}
+                        handleSignUp={handleSignUp}
+                        setHasAccount={setHasAccount}
+                        firstName={firstName}
+                        setFirstName={setFirstName}
+                        lastName={lastName}
+                        setLastName={setLastName}
+                        birthDate={birthDate}
+                        setBirthDate={setBirthDate}
+                    />
+                )}
+                {error && <p className="text-danger">{error}</p>}
+            </div>
+        </div>
+    );
+}
+
+export default AuthPage;
